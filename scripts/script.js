5ff9d7054e60ded168a34a76e76ae45acfb5c9e1
tiedej2000 <tiedejustin1@gmail.com>
2025-07-14 09:37:36 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const tiles = document.querySelectorAll(".big__tile");
     tiles.forEach(tile => observer.observe(tile));
 });
+
+ new rive.Rive({
+    src: '../media/mocca_coffee_pot.riv',
+    canvas: document.getElementById('rive-canvas'),
+    autoplay: true,
+  });
+
+//shop kaffee content
+
+const coffeeSets = {
+    item1:{
+        img1: "../media/shop/Bohne_1.png", img2: "media/Bohne_1b.png" 
+    }
+}
+//
+
+const shopCard1 = document.querySelector('.shop__card #item1')
+const nextBtn = document.querySelector('.next_item__btn')
+
+shopCard1.style.backgroundImage = `url('${coffeeSets.item1.img1}')`;
+
+
+
+//rive
+new rive.Rive({
+    src: 'mocca_coffee_pot (500x500).riv',
+    canvas: document.getElementById('rive-canvas'),
+});
