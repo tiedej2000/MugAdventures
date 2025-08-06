f842edcd21b61ac60ac8b1ce5c7cb380a1a9e11f
tiedej2000 <tiedejustin@protonmail.de>
2025-08-06 22:02:21 +0200

    adj

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const loader = document.querySelector('.loader');
 
     //disable hover effects on mobile
+    
 
     setTimeout(() => {
         loader.classList.add('hide');
@@ -356,6 +357,28 @@ const riveInstance = new rive.Rive({
 
 // delete hover effects
 
+function hasTouch() {
+  return 'ontouchstart' in document.documentElement
+         || navigator.maxTouchPoints > 0
+         || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all the :hover stylesheets
+  try { // prevent exception on browsers not supporting DOM styleSheets properly
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
 
 
 
